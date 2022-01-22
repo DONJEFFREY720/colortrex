a4dfe9450a56490c0ba7c5c9503ed82d9bc496c9
DONJEFFREY720 <95631932+DONJEFFREY720@users.noreply.github.com>
2022-01-22 15:32:09 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -96,7 +96,7 @@ function draw() {
     
     if((touches.length > 0 || keyDown("SPACE")) && trex.y  >= height-120) {
       jumpSound.play( )
-      trex.velocityY = -10;
+      trex.velocityY = -12;
        touches = [];
     }
     
@@ -183,7 +183,7 @@ function spawnObstacles() {
     }
     
     //assign scale and lifetime to the obstacle           
-    obstacle.scale = 0.3;
+    obstacle.scale = 0.5;
     obstacle.lifetime = 300;
     obstacle.depth = trex.depth;
     trex.depth +=1;
